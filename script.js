d0063b17bc74a3db5b3aaa377b1eccd0f7b9597e
ptrrdrck <56177908+ptrrdrck@users.noreply.github.com>
2023-01-17 13:04:52 -0800

    Reset unread chapters fn

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
     from characteristically distinct translations.
 **/
 
+let translations = ['Stephen Mitchell','Gia-Fu Feng & Jane English','Stephen Addiss & Stanley Lombardo','Derek Lin'];
 let selectedChapter = 1;
 let readChapters = JSON.parse(localStorage.getItem('readChapters')) || [];
 let unreadChapters = JSON.parse(localStorage.getItem('unreadChapters')) || [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81];
@@ -31,7 +32,17 @@ function shuffle(array) {
   return array;
 };
 
-let translations = ['Stephen Mitchell','Gia-Fu Feng & Jane English','Stephen Addiss & Stanley Lombardo','Derek Lin'];
+function displayUnreadChapters () {
+  document.getElementById('unread-chapters').remove();
+  let x = document.createElement('TABLE');
+  x.setAttribute('id', 'unread-chapters');
+  tablePlaceholder.appendChild(x);
+  for (var unreadChapter of unreadChapters) {
+    let y = document.createElement('TD');
+    y.appendChild(document.createTextNode(unreadChapter));
+    document.getElementById('unread-chapters').appendChild(y);    
+  };
+};
 
 /**
     Random chapter selection
@@ -60,15 +71,7 @@ function newRandomChapter () {
   });
   localStorage.setItem('unreadChapters', JSON.stringify(unreadChapters));
 
-  document.getElementById('unread-chapters').remove();
-  let x = document.createElement('TABLE');
-  x.setAttribute('id', 'unread-chapters');
-  tablePlaceholder.appendChild(x);
-  for (var unreadChapter of unreadChapters) {
-    let y = document.createElement('TD');
-    y.appendChild(document.createTextNode(unreadChapter));
-    document.getElementById('unread-chapters').appendChild(y);    
-  };
+  displayUnreadChapters();
 };
 
 newRandomChapter();
@@ -159,19 +162,21 @@ function viewChapter(chapter) {
   });
   localStorage.setItem('unreadChapters', JSON.stringify(unreadChapters));
 
-  document.getElementById('unread-chapters').remove();
-  let x = document.createElement('TABLE');
-  x.setAttribute('id', 'unread-chapters');
-  tablePlaceholder.appendChild(x);
-  for (var unreadChapter of unreadChapters) {
-    let y = document.createElement('TD');
-    y.appendChild(document.createTextNode(unreadChapter));
-    document.getElementById('unread-chapters').appendChild(y);    
-  };
+  displayUnreadChapters();
 };
 
 chapterSelectButton.addEventListener('click', () => {
   viewChapter();
+});
+
+const resetUnreadButton = document.getElementById('reset-unread-button');
+
+resetUnreadButton.addEventListener('click', () => {
+  localStorage.removeItem('readChapters');
+  localStorage.removeItem('unreadChapters');
+  readChapters = [];
+  unreadChapters = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81];
+  displayUnreadChapters();
 });
 
 /**
