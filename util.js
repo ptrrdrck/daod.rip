fd0e9c50ab9eb0ca8682d7f1998bf3116f5e5af3
ptrrdrck <56177908+ptrrdrck@users.noreply.github.com>
2023-02-27 16:22:35 -0800

    Remove commented code

--- util.js
+++ util.js
@@ -5,21 +5,6 @@
 **/
 
 /**
- * Custom cursor
- */
-/*
-document.body.addEventListener("mousemove", (evt) => {
-  const mouseX = evt.clientX;
-  const mouseY = evt.clientY;
-
-  gsap.set(".cursor", {
-    x: mouseX,
-    y: mouseY,
-  });
-});
-*/
-
-/**
  * Theme changing
  */
 const themes = {
