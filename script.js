76f15506f39792d78b104a867b3a72d48148b8d1
ptrrdrck <56177908+ptrrdrck@users.noreply.github.com>
2023-09-20 18:48:09 -0700

    Fix translation checkbox initial state

--- script.js
+++ script.js
@@ -398,6 +398,25 @@ resetUnreadButton.addEventListener("click", () => {
 /**
     Translation control
 **/
+[
+  { checkBoxId: "mitchell-checkbox", name: "Stephen Mitchell" },
+  { checkBoxId: "fengEnglish-checkbox", name: "Gia-Fu Feng & Jane English" },
+  {
+    checkBoxId: "addissLombardo-checkbox",
+    name: "Stephen Addiss & Stanley Lombardo",
+  },
+  { checkBoxId: "lin-checkbox", name: "Derek Lin" },
+  { checkBoxId: "legge-checkbox", name: "James Legge" },
+  { checkBoxId: "leguin-checkbox", name: "Ursula K. Le Guin" },
+  { checkBoxId: "lau-checkbox", name: "D. C. Lau" },
+].forEach(({ checkBoxId, name }) => {
+  if (selectedTranslations.includes(name)) {
+    localStorage.setItem(checkBoxId, "true");
+  } else {
+    localStorage.setItem(checkBoxId, "false");
+  }
+});
+
 function checkBoxes() {
   var boxes = document.querySelectorAll("input[type='checkbox']");
   for (var i = 0; i < boxes.length; i++) {
@@ -463,9 +482,4 @@ function refreshCurrentChapter() {
       JSON.stringify(selectedTranslations)
     );
   });
-  if (selectedTranslations.includes(name)) {
-    localStorage.setItem(checkBoxId, "true");
-  } else {
-    localStorage.setItem(checkBoxId, "false");
-  }
 });
